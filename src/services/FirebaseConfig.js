5ff6d6959b51158a65ac9ab8e97048c2d5f5d51e
ilohest <isaure.lohest@gmail.com>
2025-03-05 23:23:38 +0100

    Remove exposed secrets from FirebaseConfig.js

--- src/services/FirebaseConfig.js
+++ src/services/FirebaseConfig.js
@@ -4,17 +4,17 @@ import { getAuth } from "firebase/auth";
 
 // Configuration Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyATlJstf30oYZ9qm1Ta8Ix746pppu9npmg",
-    authDomain: "color-palettes-e81b4.firebaseapp.com",
-    databaseURL: "https://color-palettes-e81b4-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "color-palettes-e81b4",
-    storageBucket: "color-palettes-e81b4.firebasestorage.app",
-    messagingSenderId: "793358717098",
-    appId: "1:793358717098:web:4eafea88b2e75a3a31c0d3",
-    measurementId: "G-14XBCGL2SQ",
-    clientId: "793358717098-ihfn7p7pdq6jm02bmth8mkqr2kqgltqa.apps.googleusercontent.com"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+    clientId: import.meta.env.VITE_CLIENT_ID
 };
-
+  
 // Initialiser Firebase
 const app = initializeApp(firebaseConfig);
 
